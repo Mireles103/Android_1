aa3c119569ce43aca42ba3763df5f6ccfb876bb3
Jose Mireles <Mirelesjose379@gmail.com>
2023-01-18 19:42:58 -0600

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("My name is Jose Mireles")
+    println("I am 20 years old")
+    println("I have a Twin brother named Jesus")
+    println("My First car was a 2007 Nissan Sentra")
+    println("My favorite Hobby is collecting Japanese Figures")
+
+
+    println("Program arguments: ${args.joinToString()}")
+}
